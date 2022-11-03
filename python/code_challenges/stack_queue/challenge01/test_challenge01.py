759213adc28446ad1bdd23eff803244bdeda9d8e
Lujain92 <lujainaljarrah92@gmail.com>
2022-11-03 06:45:05 -0700

    edit stack and queue

--- python/code_challenges/stack_queue/challenge01/test_challenge01.py
+++ python/code_challenges/stack_queue/challenge01/test_challenge01.py
@@ -1,1 +1,34 @@
-# Write your test here
+from challenge01 import MyQueue
+import pytest
+
+def test_one(fixed):
+    
+    assert fixed.pop()==1
+
+def test_two(fixed):
+    
+    fixed.pop()
+    fixed.pop()
+    fixed.pop()
+    assert fixed.empty()==True
+
+def test_three(fixed):
+    
+    fixed.pop()
+    fixed.pop()
+    
+    assert fixed.empty()==False
+
+def test_four(fixed):
+    
+    assert  fixed.peek()==1
+
+
+@pytest.fixture
+def fixed():
+    myQueue=MyQueue()
+    myQueue.push(1)
+    myQueue.push(2)
+    myQueue.push(3)
+    return myQueue
+
--- python/code_challenges/stack_queue/challenge02/test_challenge02.py
+++ python/code_challenges/stack_queue/challenge02/test_challenge02.py
@@ -6,4 +6,9 @@ def test_two():
     assert isValid("()[]{}")==True
 def test_three():
     assert isValid("[({}]")==False
-
+def test_four():
+    assert isValid("[({hello})]")==True
+def test_five():
+    assert isValid("[(hello)()]")==True
+def test_six():
+    assert isValid("[{(())}]")==True
