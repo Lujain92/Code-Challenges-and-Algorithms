360ebca43cb7ba81c10199a6871368b2b6c15550
Lujain Aljarrah <109732786+Lujain92@users.noreply.github.com>
2022-12-15 23:22:43 +0300

    feat: add test

--- python/code_challenges/hashtable/challenge02/test_challenge02.py
+++ python/code_challenges/hashtable/challenge02/test_challenge02.py
@@ -1,1 +1,20 @@
 # Write your test here
+from .challenge02 import repeated
+
+def test01():
+    s="ASAC is a department at LTUC. ASAC teaches programming in LTUC."
+    expected= repeated(s)
+    actual="ASAC"
+    assert expected==actual
+    
+def test02():
+    s="I am learning programming at ASAC."
+    expected= repeated(s)
+    actual="No Repetition"
+    assert expected==actual
+
+def test03():
+    s="aa bb cc bb aa. dd"
+    expected= repeated(s)
+    actual="aa"
+    assert expected==actual
