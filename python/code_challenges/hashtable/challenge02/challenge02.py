00caed28d1a36cc4d84cfc2c93c2a4bf4fc46ea3
Lujain Aljarrah <109732786+Lujain92@users.noreply.github.com>
2022-12-15 23:06:42 +0300

    feat: add the functionality

--- python/code_challenges/hashtable/challenge02/challenge02.py
+++ python/code_challenges/hashtable/challenge02/challenge02.py
@@ -1,1 +1,12 @@
-# Write here the code challenge solution
+import re
+
+def l(s):
+  h={}
+  s = re.sub(r'[^\w\s]', '', s)
+  print(s)
+  for i in  s.split():
+    h[i]=h.get(i,0)+1
+  for i in s.split():
+    if h[i]>1:
+      return i
+  return 'no repeattion'
